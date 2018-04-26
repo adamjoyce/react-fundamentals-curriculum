4ee240d0f5780f84a0f7f7f81c57351b2c4f7070
Adam Joyce <adamjoyce@outlook.com>
2018-04-26 20:37:34 +0100

    WIP - Stage 2

--- app/components/App.js
+++ app/components/App.js
@@ -3,8 +3,14 @@ var React = require('react');
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World! x
+      <div className="container">
+        <div
+          className="home-container"
+          style={{backgroundImage: "url('app/images/pattern.svg')"}}>
+          <div className="header">
+            <h1>Main.js Header</h1>
+          </div>
+        </div>
       </div>
     );
   }
